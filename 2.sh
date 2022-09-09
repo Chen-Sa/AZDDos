1808986f7d0779806c6954b8773b2a217b1fe340
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 21:03:36 +0000

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 git clone https://github.com/Chen-Sa/hawk.git
-sh hawk/setup.sh
+sudo apt-get install nmap
+sudo apt-get install python3-pip
+
+sudo pip3 install readline
+sudo pip3 install binascii
+sudo pip3 install struct
+sudo pip3 install textwrap
+sudo pip3 install multiprocessing
+sudo pip3 install threading
+sudo pip3 install queue
+sudo pip3 install subprocess
+sudo pip3 install time
+
+sudo pip3 install colorama
+
+sudo pip3 install ipaddress
+sudo pip3 install python-nmap
+sudo pip3 install ipinfo
+sudo pip3 install scapy
+sudo pip3 install shodan
+sudo pip3 install python-whois
+sudo pip3 install paramiko
+sudo pip3 install netfilterqueue
