f05dfc0f47aad9b50c051c58220af81c50883eac
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 22:13:27 +0000

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/Chen-Sa/hawk.git
-sudo wget -O setup.sh https://raw.githubusercontent.com/Chen-Sa/hawk/master/setup.sh && chmod +x setup.sh && clear && sh setup.sh
+curl https://raw.githubusercontent.com/Chen-Sa/hawk/master/setup.sh|bash
