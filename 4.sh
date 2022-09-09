50ad5ca2d520a5368a729ad57011e759a56b0d72
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 22:04:23 +0000

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 git clone https://github.com/Chen-Sa/hawk.git
-sudo apt-get update
-sudo apt-get install nmap
-curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-
-sudo pip3 install readline
-sudo pip3 install binascii
-sudo pip3 install struct
-sudo pip3 install textwrap
-sudo pip3 install multiprocessing
-sudo pip3 install threading
-sudo pip3 install queue
-sudo pip3 install subprocess
-sudo pip3 install time
-
-sudo pip3 install colorama
-
-sudo pip3 install ipaddress
-sudo pip3 install python-nmap
-sudo pip3 install ipinfo
-sudo pip3 install scapy
-sudo pip3 install shodan
-sudo pip3 install python-whois
-sudo pip3 install paramiko
-sudo pip3 install netfilterqueue
-cd hawk/
-sudo python3 hawk.py
+wget -O setup.sh https://raw.githubusercontent.com/Chen-Sa/hawk/master/setup.sh && chmod +x setup.sh && clear && ./setup.sh
