e9f4e3421e9cd7c27fbd90ac9c675675937a1dde
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 22:20:10 +0000

    Update and rename 9.sh to 10.sh

--- 9.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git clone https://github.com/Chen-Sa/hawk.git
-wget -O setup.sh https://raw.githubusercontent.com/Chen-Sa/hawk/master/setup.sh && chmod +x setup.sh && clear && sh setup.sh
