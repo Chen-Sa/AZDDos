2a2f4109e966481e48d169a3a70d4382b8694a8d
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 19:14:57 +0000

    Update and rename 3.sh to 4.sh

--- 4.sh
+++ 4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/MatrixTM/MHDDoS.git
-wget https://raw.githubusercontent.com/Chen-Sa/AZDDos/main/get-pip.py
+curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 cd MHDDoS/
 pip install -r requirements.txt
